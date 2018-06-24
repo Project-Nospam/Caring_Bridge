240eb947bf333f496b44d95257104ac1c59edff4
Jason Phan <Phanx091@umn.edu>
2018-06-24 17:37:40 -0500

    add profile, site, and admin skeleton pages

---
+++ src/components/AdminPage/AdminPage.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+class AdminPage extends Component {
+    constructor(props) {
+        super(props);
+
+
+    }
+
+
+
+
+
+
+
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default AdminPage;
---
+++ src/components/SitePage/SitePage.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class SitePage extends Component {
+    constructor(props) {
+        super(props);
+
+
+    }
+
+
+
+
+
+
+
+
+    render() {
+        return (
+            <div>
+                <h2>SitePage</h2>
+            </div>
+        )
+    }
+}
+
+export default SitePage;
