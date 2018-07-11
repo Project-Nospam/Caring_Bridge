da63f0bf62907677c8ce013c445ac334e644ef4e
Jason Phan <Phanx091@umn.edu>
2018-07-11 16:56:28 -0500

    initial commit from fork

--- server/modules/database.js
+++ server/modules/database.js
@@ -5,28 +5,52 @@
 
 const mongoose = require('mongoose');
 
+
 /* Mongo Connection */
-let mongoURI = '';
+let mongoURI = {};
+
 
 // process.env.MONGODB_URI will only be defined if you are running on Heroku
 if (process.env.MONGODB_URI) {
+
+  let params = process.env.MONGODB_URI;
+  let auth = params.auth.split(':');
+
+  mongoURI = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  ssl: true, // heroku requires ssl to be true
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+
   // Heroku will provide this when deployed
   // use the string value of the environment variable
-  mongoURI = process.env.MONGODB_URI;
-} else if (process.env.DATABASE) {
-  // use database provided in the .env file
-  mongoURI = 'mongodb://' + process.env.DATABASE;
+  // mongoURI = process.env.MONGODB_URI;
 } else {
-  // use the local database server
-  mongoURI = 'mongodb://localhost:27017/cb_data';
+  // use database provided in the .env file
+  mongoURI = {
+    host:'mongodb://localhost',
+    port: 27017,
+    database: 'cb_data',
+    max: 10,
+    idleTimeoutMillis: 30000,
+
+  }
 }
+console.log('heroku', mongoURI);
 
-mongoose.connect(mongoURI);
+const pool = new mongoose.Pool(mongoURI);
 
-mongoose.connection.once('open', () => {
+pool.connect(mongoURI);
+
+pool.connection.once('open', () => {
   console.log('Mongo connected');
 });
 
-mongoose.connection.on('error', (err) => {
+pool.connection.on('error', (err) => {
   console.log('Error on mongoose connection: ', err);
 });
